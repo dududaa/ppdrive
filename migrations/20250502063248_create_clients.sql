74de331f951c8fe55d1f03d747f98c1b8fcdac72
Ajayi Olamide <prodbyola@gmail.com>
2025-05-02 14:41:13 +0100

    using string in place of uuid

--- migrations/20250502063248_create_clients.sql
+++ migrations/20250502063248_create_clients.sql
@@ -1,8 +1,6 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE IF NOT EXISTS clients(
     id SERIAL PRIMARY KEY,
     enc_key BYTEA NOT NULL,
     enc_payload BYTEA NOT NULL,
-    cid UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4()
+    cid VARCHAR(36) UNIQUE NOT NULL
 )
