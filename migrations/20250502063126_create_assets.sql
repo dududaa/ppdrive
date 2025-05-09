b1de0c650f47d6f527febcfb6f88e9e9fe48169a
Ajayi Olamide <prodbyola@gmail.com>
2025-05-09 09:44:48 +0100

    added support for asset custom_path.

--- migrations/20250502063126_create_assets.sql
+++ migrations/20250502063126_create_assets.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS assets (
     asset_path VARCHAR(3000) NOT NULL UNIQUE,
     user_id INTEGER NOT NULL,
     public BOOLEAN DEFAULT FALSE NOT NULL,
+    custom_path VARCHAR(120),
     UNIQUE (user_id, asset_path)
 )
