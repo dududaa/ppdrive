01c3c0690bf4e594384025a2e948dab54c540830
Ajayi Olamide <prodbyola@gmail.com>
2025-05-09 16:26:44 +0100

    docs: fix build error

--- ppdocs/docusaurus.config.ts
+++ ppdocs/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/ppdrive.png",
 
   // Set the production url of your site here
-  url: "https://prodbyola.github.io/ppdrive",
+  url: "https://prodbyola.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/ppdrive",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
