4cc008aebd8619c322ba32dcfa85d5f131c8c433
Ajayi Olamide <prodbyola@gmail.com>
2025-05-09 16:22:16 +0100

    added docs deployment.

--- ppdocs/docusaurus.config.ts
+++ ppdocs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: "img/ppdrive.png",
 
   // Set the production url of your site here
-  url: "https://ppdrive.github.io",
+  url: "https://prodbyola.github.io/ppdrive",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
