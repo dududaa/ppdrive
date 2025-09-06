d2db1a701ad25e428383889bebf0ca5ceb4155d2
Ajayi Olamide <prodbyola@gmail.com>
2025-09-06 14:04:32 +0100

    fixed SIGSEGV

--- services/rest/src/app.rs
+++ services/rest/src/app.rs
@@ -4,13 +4,8 @@ use axum::http::header::{
     ACCEPT, ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_ORIGIN, AUTHORIZATION, CONTENT_TYPE,
 };
 use axum::http::{HeaderName, HeaderValue};
-use axum::{
-    extract::MatchedPath,
-    http::Request,
-    routing::{get, IntoMakeService},
-    Router,
-};
-use handlers::plugin::router::ServiceRouter;
+use axum::{extract::MatchedPath, http::Request, routing::get, Router};
+use handlers::plugin::router::{ServiceRouter, SharedRouter};
 use ppd_shared::opts::{ServiceAuthMode, ServiceConfig, ServiceType};
 use tower_http::cors::{AllowOrigin, Any};
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
@@ -52,7 +47,7 @@ fn to_origins(origins: &Option<Vec<String>>) -> AllowOrigin {
     }
 }
 
-async fn create_app(config: &ServiceConfig) -> Result<IntoMakeService<Router<()>>, ServerError> {
+async fn serve_app(config: &ServiceConfig) -> ServerResult<()> {
     let state = HandlerState::new(config).await?;
     let origins = &config.base.allowed_origins;
 
@@ -69,39 +64,48 @@ async fn create_app(config: &ServiceConfig) -> Result<IntoMakeService<Router<()>
         .allow_methods(Any);
 
     set_var(BEARER_KEY, BEARER_VALUE);
+    if let Ok(client_router) = get_client_router(config) {
+        let router = Router::new()
+            .route("/:asset_type/*asset_path", get(get_asset))
+            .nest("/client", client_router.as_ref().clone())
+            .layer(
+                TraceLayer::new_for_http().make_span_with(|request: &Request<_>| {
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+    
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                }),
+            )
+            .layer(cors)
+            .with_state(state)
+            .into_make_service();
+    
+        match tokio::net::TcpListener::bind(format!("0.0.0.0:{}", &config.base.port)).await {
+            Ok(listener) => {
+                if let Ok(addr) = listener.local_addr() {
+                    tracing::info!("new service listening on {addr}");
+                }
+    
+                axum::serve(listener, router)
+                    .await
+                    .map_err(|err| ServerError::InitError(err.to_string()))?;
+            }
+            Err(err) => {
+                tracing::error!("Error starting listener: {err}");
+                panic!("{err}")
+            }
+        }
+    }
 
-    println!("before loading router");
-    let client_router = get_client_router(config)?;
 
-    println!(
-        "after loading router, has routes {}",
-        client_router.has_routes()
-    );
-
-    let router = Router::new()
-        .route("/:asset_type/*asset_path", get(get_asset))
-        .nest("/client", Router::new())
-        .layer(
-            TraceLayer::new_for_http().make_span_with(|request: &Request<_>| {
-                let matched_path = request
-                    .extensions()
-                    .get::<MatchedPath>()
-                    .map(MatchedPath::as_str);
-
-                info_span!(
-                    "http_request",
-                    method = ?request.method(),
-                    matched_path,
-                    some_other_field = tracing::field::Empty,
-                )
-            }),
-        )
-        .layer(cors)
-        .with_state(state)
-        .into_make_service();
-
-    println!("router created...");
-    Ok(router)
+    Ok(())
 }
 
 type LoggerGuard = tracing_appender::non_blocking::WorkerGuard;
@@ -126,35 +130,39 @@ pub fn start_logger() -> ServerResult<LoggerGuard> {
     Ok(guard)
 }
 
-pub async fn initialize_app(config: &ServiceConfig) -> ServerResult<IntoMakeService<Router<()>>> {
+pub async fn initialize_app(config: &ServiceConfig) -> ServerResult<()> {
     // start ppdrive app
     init_secrets().await?;
-    create_app(&config).await
+    serve_app(&config).await
 }
 
-fn get_client_router(config: &ServiceConfig) -> ServerResult<Router<HandlerState>> {
+fn get_client_router(config: &ServiceConfig) -> ServerResult<SharedRouter> {
     let max_upload_size = config.base.max_upload_size;
-    let router = if config.auth.modes.contains(&ServiceAuthMode::Client) {
+    if config.auth.modes.contains(&ServiceAuthMode::Client) {
         let svc_router = ServiceRouter {
             svc_type: ServiceType::Rest,
             auth_mode: ServiceAuthMode::Client,
         };
 
         println!("calling router get...");
-        svc_router.get(max_upload_size).map(|r| r.as_ref().clone())?
-    } else {
-        Router::new()
-    };
+        let r = svc_router
+            .get(max_upload_size)?;
 
-    Ok(router)
+        Ok(r)
+    } else {
+        Err(ServerError::InternalError("unable to load client router".to_string()))
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use handlers::plugin::router::ServiceRouter;
-    use ppd_shared::{opts::{ServiceAuthMode, ServiceConfig}, plugin::Plugin};
+    use ppd_shared::{
+        opts::{ServiceAuthMode, ServiceConfig},
+        plugin::Plugin,
+    };
 
-    use crate::{app::create_app, ServerResult};
+    use crate::{app::serve_app, ServerResult};
 
     #[tokio::test]
     async fn test_create_app() -> ServerResult<()> {
@@ -166,9 +174,9 @@ mod tests {
         config.base.port = 5000;
         config.auth.modes.push(ServiceAuthMode::Client);
 
-        let ca = create_app(&config).await;
-        if let Err(err) = &ca  {
-            println!("err: {err}")    
+        let ca = serve_app(&config).await;
+        if let Err(err) = &ca {
+            println!("err: {err}")
         }
 
         assert!(ca.is_ok());
--- services/rest/src/lib.rs
+++ services/rest/src/lib.rs
@@ -14,33 +14,15 @@ async fn launch_svc(config: Arc<ServiceConfig>) -> ServerResult<()> {
     let _guard = start_logger()?;
     
     run_migrations(&config.base.db_url).await?;
-    let app = initialize_app(&config).await?;
-
-    match tokio::net::TcpListener::bind(format!("0.0.0.0:{}", &config.base.port)).await {
-        Ok(listener) => {
-            if let Ok(addr) = listener.local_addr() {
-                tracing::info!("new service listening on {addr}");
-            }
-
-            axum::serve(listener, app)
-                .await
-                .map_err(|err| ServerError::InitError(err.to_string()))?;
-        }
-        Err(err) => {
-            tracing::error!("Error starting listener: {err}");
-            panic!("{err}")
-        }
-    }
+    initialize_app(&config).await?;
 
     Ok(())
 }
 
 #[no_mangle]
 pub extern "C" fn start_svc(cfg_raw: *const ServiceConfig) {
-    println!("loading svc config raw...");
     let config = unsafe { Arc::from_raw(cfg_raw) };
     
-    println!("config raw loaded...");
     match Runtime::new() {
         Ok(rt) => {
             rt.block_on(async {
