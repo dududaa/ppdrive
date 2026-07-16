678d27a36e3a7d59d93b8101761d61a8012893da
Ajayi Olamide <prodbyola@gmail.com>
2026-07-16 02:46:53 +0100

    Any test successful

--- shared/src/db.rs
+++ shared/src/db.rs
@@ -1,6 +1,8 @@
 use std::ops::Deref;
+use std::str::FromStr;
 use sqlx::{AnyPool, migrate};
 use sqlx::any::install_default_drivers;
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
 
 pub type DbPool = AnyPool;
 
@@ -13,16 +15,17 @@ pub struct Database {
 impl Database {
     pub async fn new(url: &str) -> anyhow::Result<Self> {
         install_default_drivers();
-        let pool = AnyPool::connect(url).await?;
-        
+
         let engine = if url.starts_with("postgres") {
             DbEngine::Postgres
         } else if url.starts_with("mysql") {
             DbEngine::Mysql
         } else {
+            create_sqlite(url).await?;
             DbEngine::Sqlite
         };
 
+        let pool = AnyPool::connect(url).await?;
         migrate!("../migrations").run(&pool).await?;
         Ok(Self { pool, engine })
     }
@@ -48,4 +51,13 @@ pub enum DbEngine {
     Sqlite,
     Postgres,
     Mysql,
+}
+
+/// Forces the creation of sqlite file (in case the URL is a file)
+async fn create_sqlite(url: &str) -> anyhow::Result<()> {
+    let options = SqliteConnectOptions::from_str(url)?
+        .create_if_missing(true);
+
+    let _ = SqlitePoolOptions::new().connect_with(options).await?;
+    Ok(())
 }
