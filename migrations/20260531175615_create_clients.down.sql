a788a826736ee534b2aeccefdc186a1314c33eee
Ajayi Olamide <prodbyola@gmail.com>
2026-07-21 08:26:13 +0100

    migrations updated

--- migrations/20260531175615_create_clients.down.sql
+++ migrations/20260531175615_create_clients.down.sql
@@ -1,1 +1,7 @@
+DROP INDEX idx_buckets_asset_owner;
+DROP TABLE buckets;
+DROP INDEX idx_users_asset_owner;
+DROP TABLE users;
+DROP INDEX idx_clients_asset_owner;
 DROP TABLE clients;
+DROP TABLE asset_owner;
--- migrations/20260531175615_create_clients.up.sql
+++ migrations/20260531175615_create_clients.up.sql
@@ -1,9 +1,41 @@
+CREATE TABLE asset_owner
+(
+    id   INTEGER PRIMARY KEY,
+    name SMALLINT
+);
+
 CREATE TABLE clients
 (
-    id    INTEGER PRIMARY KEY,
-    pid   TEXT UNIQUE,
-    key TEXT UNIQUE,
-    name TEXT NOT NULL,
+    id              INTEGER PRIMARY KEY,
+    pid             TEXT UNIQUE,
+    key             TEXT UNIQUE,
+    name            TEXT    NOT NULL,
     max_bucket_size REAL,
-    created_at TEXT NOT NULL
-)
+    owner_id        INTEGER NOT NULL,
+    created_at      TEXT    NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES asset_owner (id) ON DELETE CASCADE
+);
+CREATE INDEX idx_clients_asset_owner ON clients (owner_id);
+
+CREATE TABLE users
+(
+    id         INTEGER PRIMARY KEY,
+    email      TEXT    NOT NULL,
+    password   TEXT    NOT NULL,
+    owner_id   INTEGER NOT NULL,
+    created_at TEXT    NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES asset_owner (id) ON DELETE CASCADE
+);
+CREATE INDEX idx_users_asset_owner on users (owner_id);
+
+CREATE TABLE buckets
+(
+    id         INTEGER PRIMARY KEY,
+    pid        TEXT UNIQUE,
+    size       REAL,
+    accepts    TEXT,
+    created_at TEXT    NOT NULL,
+    owner_id   INTEGER NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES asset_owner (id) ON DELETE CASCADE
+);
+CREATE INDEX idx_buckets_asset_owner on buckets (owner_id);
